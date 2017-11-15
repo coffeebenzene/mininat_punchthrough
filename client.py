8c0ec3199649ae79dd240d91e176ac571738a07e
coffeebenzene <ericteo_no1@hotmail.com>
2017-11-15 22:03:55 +0800

    Add partial gui (untested)

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import logging
 
 import Tkinter as tkr
 import ttk
+import ScrolledText
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -18,13 +19,62 @@ STATEMAP = {0:"    INIT",
             "SEND-RDY":2,
            }
 
-class Application(Frame):
-    pass
+class Application(ttk.Frame):
+    
+    def __init__(self, master=None):
+        ttk.Frame.__init__(self, master)
+        
+        self.textarea = ScrolledText.ScrolledText(self)
+        self.textarea.grid(row=0, column=0, columnspan=2, sticky="NSEW")
+        self.textarea.configure(state=tkr.DISABLED)
+        
+        self.input_val = tkr.StringVar()
+        self.input_field = ttk.Entry(self, text = self.input_val)
+        self.input_field.grid(row=1, column=0, sticky="SEW", pady=1)
+        self.input_field.bind("<Return>", self.send)
+        
+        self.sendbtn = ttk.Button(self, text="Send", command=self.send, state=tkr.DISABLED)
+        self.sendbtn.grid(row=1, column=1, sticky="NSEW", pady=1, padx=1)
+        
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.grid(sticky="NSEW")
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.master.title("NAT Punchthrough")
+    
+    def insert_text(self, text):
+        self.textarea.configure(state=tkr.NORMAL)
+        self.textarea.insert(tkr.END, text)
+        self.textarea.insert(tkr.END, "\n")
+        self.textarea.configure(state=tkr.DISABLED)
+    
+    def allow_sending(self, sock, dest_addr):
+        self.sock=sock
+        self.dest_addr = dest_addr
+        self.sendbtn["state"] = tkr.NORMAL
+    
+    def send(self, event=None):
+        print event # DEBUG
+
+class socket_wrapper(object):
+    
+    def __init__(self, sock, dest_addr, punch_key):
+        self.sock = sock
+        self.dest_addr = dest_addr
+        self.punch_key = punch_key
+        self.state = 2
+        self.seq_num = 0
+        self.ack_num = 0
+    
+    def send(self, payload):
+        msg = "" # NEED TO FIGURE OUT HOW TIMEOUT A SEND MESSAGE, so can retry sending.
+        
 
 
 
 # Threading related
-class AtomicVariable():
+class AtomicVariable(object):
     """For extra security"""
     
     def __init__(self, initial_value=None):
@@ -63,11 +113,15 @@ def keepalive(sock, ka_box, ka_interval, next_time):
 
 
 
-# Main
+# Main (Receiver thread.)
 def main(room_id=0, server_address=("3.0.0.2",80)):
     if not (0 <= room_id <= 999999):
         raise ValueError("Room ID must be a 6 digit number (0-999999).")
     room_id = "{:06}".format(room_id)
+    
+    # Setup Application in another thread.
+    app = Application(master=tkr.Tk())
+    app_thread = threading.Thread(target=app.mainloop)
     
     # UDP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -77,8 +131,16 @@ def main(room_id=0, server_address=("3.0.0.2",80)):
     ka_interval = AtomicVariable(10)
     next_time = AtomicVariable(time.time())
     # Keepalive is also the request to server for connection. Until ka_box is changed.
-    ka_thread = threading.Thread(target=keepalive, args=(s, ka_box, ka_interval))
+    ka_thread = threading.Thread(target=keepalive, args=(s, ka_box, ka_interval, next_time))
     ka_thread.start()
+    
+    # Allow closing of window to close everything.
+    def closewrapper():
+        ka_interval.set(-1)
+        app.destroy()
+        s.shutdown(socket.SHUT_RDWR)
+    app.protocol("WM_DELETE_WINDOW", closewrapper)
+    app.insert(">>>> Waiting for server to provide peer.")
     
     # Server response format:
     # {requester ip}\n{requester port}\n{other ip}\n{other port}\n{random key}
@@ -92,6 +154,8 @@ def main(room_id=0, server_address=("3.0.0.2",80)):
     other_addr = (data[2], int(data[3]))
     punch_key = data[4] # Assume punch_key is length 8. Not checked.
     
+    app.insert(">>>> Connecting to {}...".format(other_addr))
+    app.insert('>>>> Punch key : "{}"...'.format(punch_key))
     logger.info('Recevied from server:\n{}\n{}\n"{}"'.format(my_addr, other_addr, punch_key))
     
     # Handshaking. Can be made more robust, but for the sake of simplicity:
@@ -99,7 +163,7 @@ def main(room_id=0, server_address=("3.0.0.2",80)):
     # state between 2 clients is 1 (i.e. No state 0 and state 2.)
     # Also, clients only send actual data when state is 2.
     state = 0
-    hs_payload = "{}{}".format(punch_key, STATEMAP[state])
+    hs_payload = punch_key + STATEMAP[state]
     ka_box.set((hs_payload,other_addr))
     ka_interval.set(1)
     while state<2:
@@ -114,12 +178,21 @@ def main(room_id=0, server_address=("3.0.0.2",80)):
             hs_payload = "{}{}".format(punch_key, STATEMAP[state])
             ka_box.set((hs_payload,other_addr))
             logger.info("Changed from state {} to {}".format(prev_state, state))
+    ka_interval.set(10)
     
-    # There may be data sent in the previous message if givenstate is 3 (i.e. state from other client is 2). Check.
-    pass
+    app.insert(">>>> Connected.")
     
-    # Receive and send data.
+    # There may be data sent in the previous message if state from other client is 2.
+    # To guard against this, simply try to process data first.
+    # Process and Receive data.
+    while ka_interval.get() > 0:
+        # process data here
+        data, address = s.recvfrom(65507)
+        if address != other_addr or data[0:8] != punch_key:
+            continue
     
+    # End
+    ka_interval.set(-1)
 
 
 
