2a97aba3dc06a792f469f9339c2672a9f31585a8
zanette <zanette_cheng@mymail.sutd.edu.sg>
2017-11-15 15:16:11 +0800

    server done?

--- server.py
+++ server.py
@@ -1,35 +1,40 @@
-import socket, random
+import socket, random, time
 
-SERVER_IP = "3.0.0.1"
+SERVER_IP = "3.0.0.2"
 PORT = 1024
 
-information = [] #array of addresses
-room_id = []
+information = {} #array of addresses
+
 def main():
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #declare is internet and UDP connection
     sock.bind((SERVER_IP,PORT))
     while True:
-        data, address = sock.recvfrom(1024)
-        ip_add = address[0]
-        port = address[1]
-        if len(information) != 2:
-            information.append(address)
-            room_id.append(data)
-        #the second time a room ID is received
-        if len(room_id) == 2:
-            #combine source from h1 and h2
-            #generate the key
-            key = random.randint(1000,9999) #4 digit key
+        room_id,address = sock.recvfrom(1024) #6
+        # ip_add = address[0]
+        # port = address[1]
+        if room_id not in information:
+            d[room_id] = address
+            timeout = time.time() + 300 #start the timeout
 
-        '''
-        a chunk that timesout if there are no more request 
-        '''
+        #the second time a room ID is received aka there's a request from h2
+        else:
+            if time.time() > timeout:
+                information.clear() #delete the room and try again
+            else:
+                #combine source from h1 and h2
+                #generate the key
+                key = random.randint(0,999999)
+                key = "%06d" %(key)
 
-        #send the information to each host
-        payload_forh1 = str(address[1]) #pseudo code to convert addresses into messages that can be sent
-        sock.sendto(payload_forh1,(address[0][0],address[0][1]))
-        payload_forh2 = str(address[0])
-        sock.sendto(payload_forh2, (address[1][0], address[1][1]))
+                #concanate the two address together to send to the client
+                data_for_second_host = address[0] + '\n' + address[1] + '\n' + information[room_id][0] + '\n' + information[room_id][1] + str(key)
+                data_for_first_host = information[room_id][0] + '\n' + information[room_id][1] + address[0] + '\n' + address[1] + '\n' + str(key)
+
+                #send the information to each host
+
+                sock.sendto(data_for_first_host,information[room_id])
+
+                sock.sendto(data_for_second_host,address)
 
 
 
