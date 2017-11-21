a28212c8875568a1d3137537d9efb79d309cc18a
Amish Bhandari <amish995@hotmail.com>
2017-11-21 21:33:33 +0800

    File transfer added

--- client.py
+++ client.py
@@ -22,44 +22,59 @@ STATEMAP = {0:"    INIT",
            }
 
 class Application(ttk.Frame):
-    
+
     def __init__(self, master=None):
         ttk.Frame.__init__(self, master)
-        
+
         self.textarea = ScrolledText.ScrolledText(self)
         self.textarea.grid(row=0, column=0, columnspan=2, sticky="NSEW")
         self.textarea.configure(state=tkr.DISABLED)
         self.textarea_lock = threading.Lock()
-        
+
         self.input_val = tkr.StringVar()
         self.input_field = ttk.Entry(self, text = self.input_val)
         self.input_field.grid(row=1, column=0, sticky="SEW", pady=1)
         self.input_field.bind("<Return>", self.send)
-        
+
         self.sendbtn = ttk.Button(self, text="Send", command=self.send, state=tkr.DISABLED)
         self.sendbtn.grid(row=1, column=1, sticky="NSEW", pady=1, padx=1)
-        
+
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         self.grid(sticky="NSEW")
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
         self.master.title("NAT Punchthrough")
-    
+
     def insert_text(self, text):
         with self.textarea_lock:
             self.textarea.configure(state=tkr.NORMAL)
             self.textarea.insert(tkr.END, text)
             self.textarea.insert(tkr.END, "\n")
             self.textarea.configure(state=tkr.DISABLED)
-    
+
     def allow_sending(self, sendqueue, send_semaphore):
         self.sendqueue = sendqueue
         self.send_semaphore = send_semaphore
         self.sendbtn["state"] = tkr.NORMAL
-    
+
     def send(self, event=None):
-        self.sendqueue.append((self.input_field.get(), "MSG ", "FULL", "00000000"))
+        usr_input = self.input_field.get()
+        usr_inp_list = usr_input.split(" ")
+        if usr_inp_list[0] == ">sendfile":
+            msg_type = "FILE"
+            try:
+                with open(usr_inp_list[1]) as f:
+                    l = usr_inp_list[1]
+                    l += "|"
+                    l += f.read()
+            except IOError:
+                app.insert_text("Error: File could not be read")
+        else:
+            msg_type = "MSG "
+            l = usr_input
+
+        self.sendqueue.append((l, msg_type, "FULL", "00000000"))
         self.input_val.set("")
         self.send_semaphore.release()
 
@@ -68,16 +83,16 @@ class Application(ttk.Frame):
 # Threading related
 class AtomicVariable(object):
     """For extra security"""
-    
+
     def __init__(self, initial_value=None):
         self._lock = threading.Lock()
         self._value = initial_value
-    
+
     def set(self, value):
         with self._lock:
             self._value = value
         return value
-    
+
     def get(self):
         with self._lock:
             value = self._value
@@ -90,10 +105,10 @@ def keepalive(sock, ka_box, ka_interval, next_time):
         # Send keepalive/periodic message.
         payload, address = ka_box.get()
         sock.sendto(payload, address)
-        
+
         # Set next keepalive
         next_time.set(time.time() + ka_interval.get())
-        
+
         # Sleep until next keepalive, polling for changes in next_time every 1s.
         # (since python can't interrupt nicely.)
         while next_time.get() > time.time():
@@ -107,7 +122,7 @@ def punchthrough_receive(app, room_id, server_address):
     # UDP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.settimeout(1.0) # Make thread interruptable
-    
+
     # Setup keepalive variables
     ka_box = AtomicVariable((room_id, server_address))
     ka_interval = AtomicVariable(10)
@@ -115,7 +130,7 @@ def punchthrough_receive(app, room_id, server_address):
     # punchthrough_receive() exclusively uses keepalive thread for sending data via ka_box.
     # Initially request to server for connection.
     threading.Thread(target=keepalive, args=(s, ka_box, ka_interval, next_time)).start()
-    
+
     # Allow closing of window to close everything.
     def closewrapper():
         ka_interval.set(-1)
@@ -124,7 +139,7 @@ def punchthrough_receive(app, room_id, server_address):
         s.close()
     app.master.protocol("WM_DELETE_WINDOW", closewrapper)
     app.insert_text(">>>> Waiting for server to provide peer.")
-    
+
     # Server response format:
     # {requester ip}\n{requester port}\n{other ip}\n{other port}\n{random key}
     while True:
@@ -134,16 +149,16 @@ def punchthrough_receive(app, room_id, server_address):
             continue
         if address == server_address:
             break
-    
+
     data = data.split("\n")
     my_addr = (data[0], int(data[1]))
     other_addr = (data[2], int(data[3]))
     punch_key = data[4] # Assume punch_key is length 8. Not checked.
-    
+
     app.insert_text(">>>> Connecting to {}...".format(other_addr))
     app.insert_text('>>>> Punch key : "{}"...'.format(punch_key))
     logger.info('Recevied from server:\n{}\n{}\n"{}"'.format(my_addr, other_addr, punch_key))
-    
+
     # Handshaking. Can be made more robust, but for the sake of simplicity:
     # Assume that all clients follow protocol. This means that max difference in
     # state between 2 clients is 1 (i.e. No state 0 and state 2.)
@@ -171,9 +186,9 @@ def punchthrough_receive(app, room_id, server_address):
             ka_box.set((hs_payload,other_addr))
             logger.info("Changed from state {} to {}".format(prev_state, state))
     ka_interval.set(10)
-    
+
     app.insert_text(">>>> Connected.")
-    
+
     # Allow sending on GUI and initialize sender/receiver variables
     sendqueue = collections.deque()
     send_semaphore = threading.Semaphore(0)
@@ -192,14 +207,23 @@ def punchthrough_receive(app, room_id, server_address):
                      "app" : app,
                     }
     threading.Thread(target=sender, args=(sender_params,)).start()
-    
+
     # There may be data sent in the previous message if state from other client is 2.
     # To guard against this, simply try to process data first.
     # Requires inner loop on Receive data part. (Alternative is duplicate process data part before loop.)
     # keepalive() is used for sending ACKs.
     # ASSUMES DATAGRAMS ARE RECEIVED IN ORDER.
+    def recv_file(recv_msg):
+        msg_list = recv_msg.split("|")
+        filename = msg_list[0]
+        msg_list.pop(0)
+        file_data = msg_list.join("|")
+        with open(msg_list[0], 'w+') as f:
+            f.write(file_data)
+        app.insert_text("File saved: {}".format(filename))
+
     msg_action = {"MSG " : lambda recv_msg : app.insert_text("Peer: {}".format(recv_msg)),
-                  "FILE" : lambda recv_msg : None, # ask to receive file. TODO #TODO
+                  "FILE" : lambda recv_msg : recv_file(recv_msg), # ask to receive file.
                  }
     prev_fragnum = None
     fraglist = []
@@ -212,7 +236,7 @@ def punchthrough_receive(app, room_id, server_address):
             frag_ident = data[36:40]
             fragnum = int(data[40:48])
             recv_msg = data[48:]
-            
+
             if msg_type != "KA  " and recv_seqnum == acknum.get(): # Process non-keepalive messages
                 if frag_ident=="FRAG": # Fragemented message
                     if not ( prev_fragnum == fragnum or prev_fragnum == None ):
@@ -228,7 +252,7 @@ def punchthrough_receive(app, room_id, server_address):
                         fraglist = []
                 else: # Full message
                     msg_action[msg_type](recv_msg)
-                
+
                 # Set ACK response
                 acknum.set((recv_seqnum + 1) % 100000000)
                 ka_payload = [punch_key, STATEMAP[state],
@@ -237,10 +261,10 @@ def punchthrough_receive(app, room_id, server_address):
                 ka_payload = "".join(ka_payload)
                 ka_box.set((ka_payload,other_addr))
                 next_time.set(time.time())
-            
+
             # Update other_acknum for sender (Any message, including keepalive, can unpdate ACKs)
             other_acknum.set(recv_acknum)
-        
+
         # Receive data (ignore non-NAT punchthrough datagrams)
         while ka_interval.get() > 0:
             try:
@@ -262,13 +286,13 @@ def sender(sender_params):
     acknum = sender_params["acknum"]
     first_header = sender_params["first_header"]
     app = sender_params["app"]
-    
+
     windowsize = 1
     window = collections.deque(maxlen=windowsize) # (fullmsg, seqnum) waiting for ack. fullmsg is header+message.
     timeout = 3 # timeout for window to resend in seconds
     next_timeout = time.time()
     seqnum = 99999997 # Should start from 0. Start at 99999997 to prove that handles overflow.
-    
+
     while ka_interval.get() > 0:
         # Remove ACK'd messages.
         acked_seqnum = (other_acknum.get() - 1) % 100000000
@@ -279,17 +303,17 @@ def sender(sender_params):
                 break
         for i in range(popnum):
             window.popleft()
-        
+
         # window timed out, resend
         if time.time() > next_timeout:
             for fullmsg, msg_seqnum in window:
                 s.sendto(fullmsg, other_addr)
             next_timeout = time.time() + timeout
-        
+
         # window is not full and there are messages, send more.
         if len(window) < windowsize and send_semaphore.acquire(False):
             msg, msg_type, frag_ident, fragnum = sendqueue.popleft()
-            
+
             # Message exceeds max payload size 50000B, fragment and requeue
             # (max payload size must be less than max_UDP_payload - header_size, 50K is a nice round number.)
             if frag_ident=="FULL" and len(msg) > 50000:
@@ -323,10 +347,10 @@ def main(room_id=0, server_address=("3.0.0.2",80)):
     if not (0 <= room_id <= 999999):
         raise ValueError("Room ID must be a 6 digit number (0-999999).")
     room_id = "{:06}".format(room_id)
-    
+
     app = Application(master=tkr.Tk())
     threading.Thread(target=punchthrough_receive, args=(app, room_id, server_address)).start()
-    
+
     app.mainloop()
 
 
@@ -338,5 +362,5 @@ if __name__ == "__main__":
     parser.add_argument('room', type=int, default=0, help='Room ID')
     args = parser.parse_args()
     server_address = (args.ip, args.port)
-    
+
     main(args.room, server_address)
